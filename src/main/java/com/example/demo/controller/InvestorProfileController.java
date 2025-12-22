6f48fbd70fe8f34cf7f1d5bd0b8756fb27bd5091
bala-amypo <bala@amypo.in>
2025-12-22 18:44:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/InvestorProfileController.java
@@ -0,0 +1,28 @@
+src/main/java/com/example/demo
+│
+├── DemoApplication.java
+│
+├── controller
+│   ├── 
+│   └── 
+│
+├── entity
+│   ├── 
+│   ├── 
+│   └── enums
+│       ├── 
+│       └── 
+│
+├── repository
+│   ├── InvestorProfileRepository.java
+│   └── RebalancingAlertRecordRepository.java
+│
+├── service
+│   ├── InvestorProfileService.java
+│   ├── RebalancingAlertService.java
+│   └── impl
+│       ├── InvestorProfileServiceImpl.java
+│       └── RebalancingAlertServiceImpl.java
+│
+└── exception
+    └── ResourceNotFoundException.java
