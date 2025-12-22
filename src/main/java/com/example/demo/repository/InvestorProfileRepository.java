28887ee3efed4a504041f7a01be862424c9e771b
bala-amypo <bala@amypo.in>
2025-12-22 03:17:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/InvestorProfileRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.InvestorProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface InvestorProfileRepository extends JpaRepository<InvestorProfile, Long> {
+    Optional<InvestorProfile> findByInvestorId(String investorId);
+}
