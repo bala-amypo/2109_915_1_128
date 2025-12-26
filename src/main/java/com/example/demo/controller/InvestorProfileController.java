462d6f654366fb627fc2941698f1ad62d3592330
bala-amypo <bala@amypo.in>
2025-12-26 12:13:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/InvestorProfileController.java
+++ src/main/java/com/example/demo/controller/InvestorProfileController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/investors")
 public class InvestorProfileController {
 
-    private final InvestorProfileService service;
+    private final InvestorProfileService investorProfileService;
+
 
     public InvestorProfileController(InvestorProfileService service) {
         this.service = service;
