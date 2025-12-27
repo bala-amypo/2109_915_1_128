b494bc9f6c9c292d3578ffe42ddcd3a955b37562
bala-amypo <bala@amypo.in>
2025-12-27 13:58:06 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilterConfig.java
+++ src/main/java/com/example/demo/config/JwtFilterConfig.java
@@ -1,8 +1,25 @@
 package com.example.demo.config;
 
+import com.example.demo.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JwtFilterConfig {
-    
+
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider(
+                "thisIsA32ByteMinimumSecureJwtTestKey!",
+                3600000L
+        );
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+            JwtTokenProvider jwtTokenProvider
+    ) {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
+    }
 }
