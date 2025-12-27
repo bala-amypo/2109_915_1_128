9443b278e9150f4d05564f3fecdfbc67d3d1f6ad
bala-amypo <bala@amypo.in>
2025-12-27 14:13:06 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/HoldingRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/HoldingRecordServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.HoldingRecord;
 import com.example.demo.repository.HoldingRecordRepository;
 import com.example.demo.service.HoldingRecordService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class HoldingRecordServiceImpl implements HoldingRecordService {
 
     private final HoldingRecordRepository repository;
