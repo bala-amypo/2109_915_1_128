22c007e171bc3e01d9d60217d399a5407b02ced9
bala-amypo <bala@amypo.in>
2025-12-27 06:14:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/RebalancingAlertRecord.java
+++ src/main/java/com/example/demo/entity/RebalancingAlertRecord.java
@@ -66,6 +66,10 @@ public class RebalancingAlertRecord {
     }
 
     // Getters and setters
+    public void setId(Long id) {
+    this.id = id;
+}
+
 
     public Long getId() {
         return id;
